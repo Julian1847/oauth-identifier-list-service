5fc390748cefffdbf9b6f31d83756426661df129
Julian <julian.liebert@nordakademie.de>
2025-01-13 20:19:01 +0100

    init

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("org.springframework.boot") version "3.3.6"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "2.1.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.postgresql:postgresql")
+    implementation("jakarta.validation:jakarta.validation-api")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.retry:spring-retry")
+    implementation("org.zalando:logbook-spring-boot-starter:3.10.0")
+
+    runtimeOnly("org.springframework.boot:spring-boot-starter-actuator")
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin { jvmToolchain(17) }}
