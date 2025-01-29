14fb0c35000144285f4d323aabf3a08790b6dbf7
Julian <julian.liebert@nordakademie.de>
2025-01-29 13:06:31 +0100

    add index on database and minor refactoring

--- src/main/kotlin/data/IdentifierRepository.kt
+++ src/main/kotlin/data/IdentifierRepository.kt
@@ -1,7 +1,6 @@
 package org.example.data
 
 import org.example.entity.Identifier
-import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 import java.util.*
